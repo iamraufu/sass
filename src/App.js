5452b3470aac8a6556622a4c2617dc82225109f6
Eftykhar Rahman <eftykharraufu@gmail.com>
2021-04-22 03:16:17 +0600

    scss installed and used

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import Body from './Components/Body/Body';
 import Footer from './Components/Footer/Footer';
 import Header from './Components/Header/Header';
 import Header2 from './Components/Header2/Header2';
+import Home from './Components/Home/Home';
 
 function App() {
   return (
     <div>
       <Header2/>
       <Header/>
+      <Home/>
       <Body/>
       <Footer/>
     </div>
---
+++ src/Components/Home/Home.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import './Home.scss';
+
+const Home = () => {
+      return (
+            <div>
+                  <div className="container">
+                        <h1 className="body odd">1</h1>
+                  </div>
+                  <div className="container">
+                        <h1 className="body">2</h1>
+                  </div>
+                  <div className="container">
+                        <h1 className="body odd">3</h1>
+                  </div>
+                  <div className="container">
+                        <h1 className="body">4</h1>
+                  </div>
+                  <div className="container">
+                        <h1 className="body odd">5</h1>
+                  </div>
+                  <div className="container">
+                        <h1 className="body">6</h1>
+                  </div>
+            </div>
+      );
+};
+
+export default Home;   
